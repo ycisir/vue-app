0e5793bf85101760b4df3bbdfffc0653e8f62791
yasir <mohammadyasirkmt@gmail.com>
2025-03-28 08:46:34 +0530

    list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Hello from './components/Hello.vue'
+import { ref } from 'vue'
 
 //text interpolation
 const on = false;
+
+// passing data using prop to another component
+const students = ref(['harry', 'hermoine', 'ron'])
 </script>
 
 <template>
   <HeaderSection />
   <HelloWorld class="parentClass"/>
-  <Hello />
+  <!-- <Hello v-for="n in 5" :key="n"/> -->
   <!-- <h3 class="text-red bg-black">app component</h3> -->
+
+  <!-- passing data through props -->
+  <Hello v-for="(student, i) in students" :key="i" :item="student" />
 
   <!-- global component example -->
   <!-- {{ on ? "dark mode" : "light mode" }} -->
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,118 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
-const isActive = ref(false);
-const num = ref(55);
-const type = ref("A");
-const visible = ref(false);
+// getting data through prop
+defineProps({
+	item: String
+})
 
-setTimeout(()=> {
-	isActive.value=true;
-	visible.value=true;
-},3000)
+// const isActive = ref(false);
+// const num = ref(55);
+// const type = ref("A");
+// const visible = ref(false);
+
+// setTimeout(()=> {
+// 	isActive.value=true;
+// 	visible.value=true;
+// },3000)
+
+
+
+
+// v-for --- arr
+// const students = ref(['ron', 'harry', 'hermoine', 'ginny']);
+
+
+// v-for with v-if
+const employees = ref([
+	{
+	id: "7",
+	name: "harry",
+	salary: 19000
+	},
+	{
+	id: "22",
+	name: "ron",
+	salary: 17000
+	},
+	{
+	id: "18",
+	name: "hermoine",
+	salary: 19000
+	},
+	{
+	id: "6",
+	name: "ginny",
+	salary: 15000
+	},
+])
+
+
+
+
+// arr of arrs
+// const languages = ref([
+// 	['ruby', 'java', 'python'],
+// 	['c++', 'javascript', 'php']
+// ])
+
+
+
+
+// key value
+// const course = reactive({
+// 	title: "learn vue js",
+// 	duration: "1 monts",
+// 	fees: "free"
+// })
+
+
+
+
+// obj of objs
+// const frameworks = reactive({
+// 	vuejs : {
+// 		id: 1,
+// 		title: "learn vueks"
+// 	},
+
+// 	django: {
+// 		id: 2,
+// 		title: "learn django"
+// 	},
+
+// 	ror: {
+// 		id: 3,
+// 		title: "learn ror"
+// 	},
+
+// 	laravel: {
+// 		id: 4,
+// 		title: "learn laravel"
+// 	}
+// })
+
+
+
+// computed property
+// const salaryGreaterThan = computed(()=> {
+// 	return employees.value.filter((employee) => employee.salary >= 19000);
+// })
+
+
+
+
+// method
+// function higherSalary(emps) {
+// 	return emps.filter((emp) => emp.salary >= 19000);
+// }
+
+
+
+
+
+
+
 </script>
 
 
@@ -17,11 +120,11 @@ setTimeout(()=> {
 	<!-- <h3 class="textYellow bg-black">Hello component</h3> -->
 
 	<!-- v-if directive -->
-	<h1 v-if="isActive">Authorized</h1>
-	<h1 v-else>Not authorized</h1>
+	<!-- <h1 v-if="isActive">Authorized</h1> -->
+	<!-- <h1 v-else>Not authorized</h1> -->
 
 	<!-- v-show not removed from dom just set display none if false -->
-	<h1 v-show="false">number is 5</h1>
+<!-- 	<h1 v-show="false">number is 5</h1>
 
 	<template v-if="visible">
 		<ul>
@@ -29,7 +132,112 @@ setTimeout(()=> {
 			<li>abc</li>
 			<li>abc</li>
 		</ul>
-	</template>
+	</template> -->
+
+
+
+	<!-- v-for --- key use for handle state vue recommend to use key to identify vnodes and key is special attribute-->
+
+	<!-- <h1>Honar students of houghwarts</h1>
+	<ul v-for="(student, i) in students" :key="i">
+		<li>{{ i }} --- {{ student }}</li>
+	</ul> -->
+
+
+
+
+	<!-- array of objects -->
+	<!-- <h1>Employees</h1>
+	<div v-for="(emp, i) in employees" :key="i">
+		<p>id: {{ emp.id }}</p>
+		<p>name: {{ emp.name }}</p>
+		<p>salary: {{ emp.salary }}</p>
+	</div> -->
+
+
+<!-- 	<h1>Employees</h1>
+	<div v-for="({id, name, salary}, i) in employees" :key="i">
+		<p>{{ i }}</p>
+		<p>id: {{ id }}</p>
+		<p>name: {{ name }}</p>
+		<p>salary: {{ salary }}</p>
+	</div> -->
+
+
+
+
+
+
+
+	<!-- arr of arrs -->
+
+<!-- 	<template v-for="(language, outerIndex) in languages" :key="outerIndex">
+		<h4 v-for="(item, innerIndex) in language">{{ outerIndex }}, {{innerIndex }} --- {{ item }}</h4>
+		<hr>
+	</template> -->
+
+
+
+
+	<!-- key value -->
+<!-- 		<hr>
+		<h4 v-for="(value, key, i) in course" :key="i">{{ i }} - {{ key }}: {{ value }}</h4> -->
+
+
+
+		<!-- obj of objs -->
+<!-- 		<div v-for="framework in frameworks" :key="framework.id">
+			{{ framework.id }}: {{ framework.title }} 
+		</div> -->
+
+		<!-- destructure -->
+<!-- 		<div v-for="{id, title} in frameworks" :key="id">
+			{{ id }}: {{ title }} 
+		</div> -->
+
+
+
+
+
+
+
+	<!-- <h1>Employees</h1> -->
+<!-- 	<template v-for="({id, name, salary}, i) in employees" :key="i">
+		<template v-if="salary >= 19000">
+			<p>id: {{ id }}</p>
+			<p>name: {{ name }}</p>
+			<p>salary: {{ salary }}</p>
+		</template>
+	</template> -->
+
+
+	<!-- computed with v-for -->
+<!-- 	<template v-for="({id, name, salary}, i) in salaryGreaterThan" :key="i">
+		<template v-if="salary >= 19000">
+			<p>id: {{ id }}</p>
+			<p>name: {{ name }}</p>
+			<p>salary: {{ salary }}</p>
+		</template>
+	</template> -->
+
+
+	<!-- method with v-for -->
+<!-- 	<template v-for="({id, name, salary}, i) in higherSalary(employees)" :key="i">
+		<template v-if="salary >= 19000">
+			<p>id: {{ id }}</p>
+			<p>name: {{ name }}</p>
+			<p>salary: {{ salary }}</p>
+		</template>
+	</template> -->
+
+
+
+
+	<!-- <h3>Hello component</h3> -->
+
+
+	<!-- getting data from app component through prop -->
+	<h3>data of student {{ item }}</h3>
 </template>
 
 <style>

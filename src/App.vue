7941f3659a56c85562de36c018d2c9f94e8af870
yasir <mohammadyasirkmt@gmail.com>
2025-03-29 16:40:54 +0530

    props and props validation

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,39 @@ import HelloWorld from './components/HelloWorld.vue'
 import Hello from './components/Hello.vue'
 import { ref } from 'vue'
 import FormComponent from './components/FormComponent.vue';
-
+import PropsComponent from './components/PropsComponent.vue';
+import { Person } from './services/person';
 //text interpolation
 const on = false;
 
 // passing data using prop to another component
-const students = ref(['harry', 'hermoine', 'ron'])
+// const students = ref(['harry', 'hermoine', 'ron'])
+// const fullName = ref("");
+// const firsName = ref("jack");
+// const lastName = ref("sparrow");
+
+// setTimeout(()=>{
+//   // fullName.value = "Jack sparrow";
+//   firsName.value = "jack";
+//   lastName.value = "sparrow";
+// }, 3000)
+
+
+
+// binding multiple properties using object
+// const employee = {
+//   name: "jack",
+//   company: "microsoft"
+// }
+
+
+
+
+
+// custom class 
+const person = new Person("harry", "potter");
+
+
 </script>
 
 <template>
@@ -21,13 +48,57 @@ const students = ref(['harry', 'hermoine', 'ron'])
   <!-- <Hello v-for="(student, i) in students" :key="i" :item="student" /> -->
   
   
-  <Hello />
+  <!-- <Hello /> -->
 
   <!-- global component example -->
   <!-- {{ on ? "dark mode" : "light mode" }} -->
 
-  <FormComponent />
-    
+  <!-- <FormComponent /> -->
+
+
+
+  <!-- passing props  -->
+  <!-- statis prop -->
+   <!-- one way data flow -->
+  <!-- <PropsComponent fullName="Harry potter" />  -->
+
+  <!-- dynamic prop -->
+  <!-- <PropsComponent :first-name="firsName" :last-name="lastName" />  -->
+
+
+  <!-- prop validation -->
+   <!-- use colon if want to send number type in age -->
+    <!-- <PropsComponent full-name="Harry potter" :age="21" /> -->
+    <!-- <PropsComponent :age="21" /> -->
+
+
+    <!-- boolean casting -->
+  <!-- <PropsComponent :is-logout="true" /> -->
+
+
+  <!-- arr and objects -->
+   <!-- <PropsComponent :cities="['Nagpur', 'Mumbai', 'Pune']" /> -->
+   <!-- <PropsComponent :employee="{ name: 'harry', company: 'houghwarts' }" /> -->
+
+
+   <!-- binding multiple properties using object  -->
+   <!-- <PropsComponent v-bind="employee" /> -->
+
+
+
+
+   <!-- custom validator  -->
+  <!-- <PropsComponent name="harry" :age="99" /> -->
+
+
+
+
+  <!-- custom class  -->
+  <PropsComponent :author="person"  />
+
+
+
+
 </template>
 
 <style scoped>
---
+++ src/components/PropsComponent.vue
@@ -0,0 +1,147 @@
+<script setup>
+import { computed, ref } from 'vue';
+import { Person } from '@/services/person';
+
+// declaring props 
+// const props = defineProps(["fullName"]);
+// const props = defineProps(["firstName", "lastName"]);
+// console.log(props.firstName)
+// console.log(props.lastName)
+// local data property but disconnected from future updates
+// const fname = ref(props.fullName);
+
+
+// computed property --> auto updates when prop changes
+// const fullName = computed(() => props.firstName+' '+props.lastName)
+// console.log(fullName)
+
+
+// prop validation
+// const props = defineProps({
+//     fullName: String,
+//     age: Number
+// });
+
+// const props = defineProps({
+//     fullName: {
+//         type: String,
+//         required: true,
+//         default: "John cena"
+//     },
+//     age: {
+//         type: Number,
+//         required: true
+//     }
+// });
+
+
+
+
+// boolean casting 
+// const props = defineProps({
+//     isLogout: Boolean
+// })
+
+
+
+
+
+// arr and objects
+// const props = defineProps({
+//     cities: Array,
+//     employee: Object
+// })
+
+
+
+// binding multiple properties using object
+// const props = defineProps({
+//     name: String,
+//     company: String
+// })
+
+
+
+// custom validator 
+// const props = defineProps({
+//     name: {
+//         type: String,
+//         // validator(propValue) {
+//         //     return ["harry", "hermoine", "ron", "ginny"].includes(propValue);
+//         // }
+//         validator:(propValue)=>["harry", "hermoine", "ron", "ginny"].includes(propValue),
+//     },
+//     age: {
+//         type: Number,
+//         // validator: (propValue)=> propValue>0,
+//         validator: (propValue)=> {
+//             if(propValue >= 18 && propValue <=30){
+//                 return true;
+//             }
+//             console.error("age should be between 18 and 30")
+//             return false;
+//         }
+//     }
+// })
+
+
+
+
+
+// custom class
+const props = defineProps({
+    author: Person,
+})
+
+</script>
+
+<template>
+    <h3>props</h3>
+    <p>props are custom attrs we can register in component in template we write it in kebab-case, props are readonly</p>
+    <ul>
+        <li>declaring props</li>
+        <li>passing props</li>
+        <li>using props</li>
+    </ul>
+
+    <!-- <h4>Name: {{ fullName }}</h4> -->
+    <!-- <h4>Name: {{ fullName }}</h4> -->
+
+
+    <!-- prop validation  -->
+     <!-- <h3>Name: {{ fullName }}, Age: {{ age+1 }}</h3> -->
+
+
+
+      <!-- boolean casting  -->
+       <!-- <h3>Logout: {{ isLogout }}</h3> -->
+
+
+       <!-- arr and objects -->
+        <!-- <h3 v-for="city in cities">city: {{ city }}</h3> -->
+        <!-- <h3>Name: {{ employee.name }}, company: {{ employee.company }}</h3> -->
+
+
+
+
+        <!-- binding multiple properties using object -->
+        <!-- <h3>Employee: {{ name }}, {{ company }}</h3> -->
+
+
+
+        <!-- custom validator  -->
+
+        <!-- <h3>Name: {{ name }}</h3> -->
+        <!-- <h3>Age: {{ age+4 }}</h3> -->
+        <!-- <h3>Age: {{ age }}</h3> -->
+
+
+
+
+
+        <!-- custom class  -->
+         <h3>author name: {{ author.firstName }} {{ author.lastName }}</h3>
+
+</template>
+
+<style scoped></style>
